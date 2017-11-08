d05b20695da650292639655398b2c57b3e244339
Aboli Aradhye <aboli.a.aradhye@gmail.com>
2017-11-08 23:09:13 +0530

    Express router

--- node-express/index.js
+++ node-express/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const http = require("http");
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -9,12 +11,15 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.json());
+app.use("/dishes", dishRouter);
 
 app.use((req, res, next) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
 	res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
+
 
 
 const server = http.createServer(app);
